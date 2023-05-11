b80de35af6b5f1683c030b4eb9260fc20a8f16ca
Reinier Sanders <sanders.reinier@gmail.com>
2023-05-11 15:34:37 +0200

    img format

--- js/script_photography.js
+++ js/script_photography.js
@@ -1,21 +1,26 @@
 const images = document.querySelectorAll('.img');
+const imageSources = ['img/image1.jpg', 'img/image2.jpg', 'img/image3.jpg', 'img/image4.jpg', 'img/image5.jpg', 'img/image6.jpg', 'img/image7.jpg', 'img/image8.jpg', 'img/image9.jpg', 'img/image10.jpg', 'img/image11.jpg', 'img/image12.jpg'];
 const previousImages = [];
 let currentImages = [];
 
-function getRandomImage(images) {
-  let randomIndex = Math.floor(Math.random() * images.length);
-  let randomImage = images[randomIndex];
+let imageIndex = -1;
+function getRandomImage() {
+  imageIndex++;
+  if (imageIndex >= imageSources.length) {
+    imageIndex = 0;
+  }
+  let randomImage = imageSources[imageIndex];
   if (previousImages.includes(randomImage) || currentImages.includes(randomImage)) {
-    return getRandomImage(images);
+    return getRandomImage();
   }
   return randomImage;
 }
 
-function showImages(images) {
+function showImages() {
   currentImages = [];
   for (let i = 0; i < images.length; i++) {
     let image = images[i];
-    let randomImage = getRandomImage(images);
+    let randomImage = getRandomImage();
     currentImages.push(randomImage);
     previousImages.push(randomImage);
     if (previousImages.length > 9) {
@@ -32,27 +37,7 @@ function showImages(images) {
   }
 }
 
-function loadImages() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'photography/', true);
-  xhr.responseType = 'document';
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      const files = xhr.response.getElementsByTagName('a');
-      const images = [];
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i].href;
-        if (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.JPG') || file.endsWith('.JPEG')) {
-          images.push('photography/' + file);
-        }
-      }
-      showImages(images);
-      setInterval(() => {
-        showImages(images);
-      }, 9000);
-    }
-  };
-  xhr.send();
-}
-
-loadImages();
+showImages();
+setInterval(() => {
+  showImages();
+}, 9000);
